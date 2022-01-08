3cd7e4a323617258ecc100f95d05a4a1a1d7f52c
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-01-08 14:40:29 +0000

    Removed route [/graphs]

--- routes/terminal.js
+++ routes/terminal.js
@@ -1,12 +1,12 @@
 /* * */
 /* IMPORTS */
-const router = require("express").Router();
-const { POSFeedback, validate } = require("../models/POSFeedback");
+const router = require('express').Router();
+const { POSFeedback, validate } = require('../models/POSFeedback');
 
-const Moment = require("moment");
-const MomentRange = require("moment-range");
+const Moment = require('moment');
+const MomentRange = require('moment-range');
 
-const _ = require("lodash");
+const _ = require('lodash');
 
 const moment = MomentRange.extendMoment(Moment);
 
@@ -23,8 +23,8 @@ const moment = MomentRange.extendMoment(Moment);
 /* * * * * */
 /* GET method for [/terminal/locations] */
 /* Responds with all distinct store locations present in the database. */
-router.get("/sessions", async (req, res) => {
-  await POSFeedback.distinct("session", (err, obj) => {
+router.get('/sessions', async (req, res) => {
+  await POSFeedback.distinct('session', (err, obj) => {
     res.send(obj);
   });
 });
@@ -34,8 +34,8 @@ router.get("/sessions", async (req, res) => {
 /* * * * * */
 /* GET method for [/terminal/sessions] */
 /* Responds with all distinct store locations present in the database. */
-router.get("/locations", async (req, res) => {
-  await POSFeedback.distinct("location", (err, obj) => {
+router.get('/locations', async (req, res) => {
+  await POSFeedback.distinct('location', (err, obj) => {
     res.send(obj);
   });
 });
@@ -45,94 +45,8 @@ router.get("/locations", async (req, res) => {
 /* * * * * */
 /* GET method for [/terminal/temporalscales] */
 /* Responds with all distinct store locations present in the database. */
-router.get("/timescales", async (req, res) => {
-  res.send(["Hour", "Day", "Week", "Month"]);
-});
-
-/* * */
-/* * */
-/* * * * * */
-/* GET method for [/terminal/graphs/firstQuestionAnswers] */
-/* Responds with all distinct store locations present in the database. */
-router.get("/graphs/:question", async (req, res) => {
-  // Query the database and get items matching the requested timeframe
-  const itemsInRequestedTimeframe = await POSFeedback.find({
-    timestamp: {
-      $gte: new Date(req.query.startDate),
-      $lt: new Date(req.query.endDate)
-    }
-  });
-
-  // Filter given items that match the requested store location
-  const itemsFromRequestedLocation = itemsInRequestedTimeframe.filter(item => {
-    return item.location === req.query.location;
-  });
-
-  const itemsDividedByAnswerValue = _.chain(itemsFromRequestedLocation)
-    .groupBy(req.params.question)
-    .value();
-
-  let finalArray = [];
-
-  for (let key in itemsDividedByAnswerValue) {
-    // Initialize final array
-    let data = [];
-
-    // Get a timeframe
-    const timeframe = moment.range(
-      moment(req.query.startDate),
-      moment(req.query.endDate)
-    );
-
-    // Set timescale (day, hour, minute). Default to day.
-    const timescale = req.param.timescale || "day";
-
-    // Count how many items match .timestamp for each day
-    for (let timeunit of timeframe.by(timescale)) {
-      const itemsFromThisTimeUnit = itemsDividedByAnswerValue[key].filter(
-        item => {
-          if (
-            moment(item.timestamp).isBetween(
-              timeunit,
-              moment(timeunit).add(1, timescale)
-            )
-          )
-            return item;
-        }
-      );
-
-      data.push({
-        x: timeunit.toISOString(),
-        y: itemsFromThisTimeUnit.length
-      });
-    }
-
-    // Naming the series
-    key = eval("(" + key + ")");
-
-    const icon = key.icon ? key.icon : "";
-    const label = key.label ? key.label : "";
-    const value = key.value ? key.value.toString() : "";
-
-    const keyLabelAndValueAreEqual =
-      label
-        .split(" ")
-        .join("")
-        .toLowerCase() ===
-      value
-        .split("-")
-        .join("")
-        .toLowerCase();
-
-    let name = icon + " " + label + " (" + value + ")";
-    if (keyLabelAndValueAreEqual && label) name = icon + " " + label;
-    else if (!icon && !label && !value) name = "⚪️ No Answer";
-
-    // Push final array
-    finalArray.push({ name, data });
-  }
-
-  res.send(finalArray);
+router.get('/timescales', async (req, res) => {
+  res.send(['Hour', 'Day', 'Week', 'Month']);
 });
 
 /* * */
@@ -141,25 +55,25 @@ router.get("/graphs/:question", async (req, res) => {
 /* GET method for [/POSF/score] */
 /* Updates an existing item in the database. */
 /* Responds with the updated item */
-router.get("/score/:timescale", async (req, res) => {
+router.get('/score/:timescale', async (req, res) => {
   // 1 - Timeframe
   // Query the database and get items matching the requested timeframe
   const itemsInRequestedTimeframe = await POSFeedback.find({
     timestamp: {
       $gte: new Date(req.query.startDate),
-      $lt: new Date(req.query.endDate)
-    }
+      $lt: new Date(req.query.endDate),
+    },
   });
 
   // 2 - Location
   // Filter given items that match the requested store location
-  const itemsFromRequestedLocation = itemsInRequestedTimeframe.filter(item => {
+  const itemsFromRequestedLocation = itemsInRequestedTimeframe.filter((item) => {
     return item.location === req.query.location;
   });
 
   // 3 - Score
   // Filter given items that match the requested score value
-  const itemsWithRequestedScore = itemsFromRequestedLocation.filter(item => {
+  const itemsWithRequestedScore = itemsFromRequestedLocation.filter((item) => {
     return item.score === req.query.score;
   });
 
@@ -171,29 +85,20 @@ router.get("/score/:timescale", async (req, res) => {
   const data = [];
 
   // Get a timeframe
-  const timeframe = moment.range(
-    moment(req.query.startDate),
-    moment(req.query.endDate)
-  );
+  const timeframe = moment.range(moment(req.query.startDate), moment(req.query.endDate));
 
   // Set timescale (day, hour, minute). Default to day.
-  const timescale = req.param.timescale || "day";
+  const timescale = req.param.timescale || 'day';
 
   // Count how many items match .timestamp for each day
   for (let timeunit of timeframe.by(timescale)) {
-    const itemsFromThisTimeUnit = itemsWithRequestedScore.filter(item => {
-      if (
-        moment(item.timestamp).isBetween(
-          timeunit,
-          moment(timeunit).add(1, timescale)
-        )
-      )
-        return item;
+    const itemsFromThisTimeUnit = itemsWithRequestedScore.filter((item) => {
+      if (moment(item.timestamp).isBetween(timeunit, moment(timeunit).add(1, timescale))) return item;
     });
 
     data.push({
       x: timeunit.toISOString(),
-      y: itemsFromThisTimeUnit.length
+      y: itemsFromThisTimeUnit.length,
     });
   }
 
@@ -209,7 +114,7 @@ router.get("/score/:timescale", async (req, res) => {
 /* Stores the image in storage. */
 /* Creates a new item in the database. */
 /* Responds with the newly created item */
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   /* Validation */
   // Validate the request
   // Continue if no errors are found
@@ -235,17 +140,13 @@ router.post("/", async (req, res) => {
 /* PUT method for [/POSF/:id] */
 /* Updates an existing item in the database. */
 /* Responds with the updated item */
-router.put("/:id", async (req, res) => {
+router.put('/:id', async (req, res) => {
   // Validate the request
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   // Try saving to the database
-  const item = await POSFeedback.findByIdAndUpdate(
-    req.params.id /* Which item to update */,
-    req.body /* What is to change */,
-    { new: true } /* Respond with the updated document */
-  );
+  const item = await POSFeedback.findByIdAndUpdate(req.params.id /* Which item to update */, req.body /* What is to change */, { new: true } /* Respond with the updated document */);
 
   res.send(item);
 });
@@ -255,10 +156,10 @@ router.put("/:id", async (req, res) => {
 /* * * * * */
 /* GET method for [/api/books/:id] */
 /* Responds with a specific item from the database */
-router.get("/:id", async (req, res) => {
+router.get('/:id', async (req, res) => {
   const item = await POSFeedback.findById(req.params.id);
   if (item) res.send(item);
-  else res.status(404).send("No POSFeedback with the given ID was found.");
+  else res.status(404).send('No POSFeedback with the given ID was found.');
 });
 
 /* * */
