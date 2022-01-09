1e284acff25ebc3890e7dbfc984c936ab105b555
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-01-09 23:07:45 +0000

    Removed Winston
    For the scope of this project, a simple console.log() is enough.

--- middleware/error.js
+++ middleware/error.js
@@ -1,6 +1,10 @@
-const winston = require("winston");
+'use strict';
 
-module.exports = function(err, req, res, next) {
-  winston.error(err.message, err);
-  res.status(500).send("Something failed.");
+/* * * * * */
+/* ERROR HANDLING */
+/* * */
+
+module.exports = function (err, req, res, next) {
+  console.log(err.message, err);
+  res.status(500).send('Something failed.');
 };
